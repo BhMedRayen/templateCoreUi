b1925620d350f5d364ccb31931049a3e4ade7f47
BhMedRayen <rayenmohamed2310@gmail.com>
2024-03-21 21:22:48 +0100

    project module

---
+++ src/app/views/projects/project.module.ts
@@ -0,0 +1,6 @@
+export interface Project {
+    title: string;
+    description: string;
+    technologies: string[];
+    done: boolean;
+  }
--- src/app/views/projects/projects.module.ts
+++ src/app/views/projects/projects.module.ts
@@ -1,16 +1,54 @@
 import { NgModule } from '@angular/core';
 import {ProjectsRoutingModule } from './projects-routing.module'
+import { Project } from '../projects/project.module'; // Import Project interface here
 
 @NgModule({
   imports: [
- 
-
     ProjectsRoutingModule
-
   ],
   declarations: [
-  
   ]
 })
 export class ProjectsModule {
+  projects: Project [] = [
+    {
+      title: 'Project 1',
+      description: 'Description for Project 1',
+      technologies: ['Angular', 'Spring Boot'],
+      done: false
+    },
+    {
+      title: 'Project 2',
+      description: 'Description for Project 2',
+      technologies: ['React', 'Node.js'],
+      done: true
+    },
+    {
+      title: 'Project 3',
+      description: 'Description for Project 3',
+      technologies: ['React', 'Node.js'],
+      done: true
+    },
+    {
+      title: 'Project 4',
+      description: 'Description for Project 4',
+      technologies: ['React', 'Node.js'],
+      done: false
+    },
+    {
+      title: 'Project 5',
+      description: 'Description for Project 5',
+      technologies: ['React', 'Node.js'],
+      done: true
+    },
+    {
+      title: 'Project 6',
+      description: 'Description for Project 6',
+      technologies: ['React', 'Node.js'],
+      done: false
+    },
+
+  ];
+
+  constructor() { }
 }
