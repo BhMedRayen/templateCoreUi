5751dc63ec840ea9ff01f64f966d5a0766fd9547
BhMedRayen <rayenmohamed2310@gmail.com>
2024-03-21 23:45:12 +0100

    app

--- src/app/views/projects/projectslist/projectslist.component.ts
+++ src/app/views/projects/projectslist/projectslist.component.ts
@@ -1,12 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ProjectsModule } from '../projects.module';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-projectslist',
   standalone: true,
-  imports: [],
+  imports: [CommonModule], 
   templateUrl: './projectslist.component.html',
-  styleUrl: './projectslist.component.scss'
+  styleUrls: ['./projectslist.component.scss']
 })
-export class ProjectslistComponent {
+export class ProjectslistComponent implements OnInit {
+
+  totalProjects: number =0;
+  doneProjects: number =0;
+  undoneProjects: any[] = [];
+  showAllUndoneProjects: boolean = false;
+  constructor(private projectsModule: ProjectsModule) {}
+  toggleAllUndoneProjects() {
+    this.showAllUndoneProjects = !this.showAllUndoneProjects;
+    if (this.showAllUndoneProjects) {
+        this.undoneProjects = this.projectsModule.projects.filter(project => !project.done).slice(0, 6);
+    } else {
+        this.undoneProjects = this.projectsModule.projects.filter(project => !project.done);
+    }
+}
+ngOnInit(): void {
+    this.totalProjects = this.projectsModule.projects.length;
+    this.doneProjects = this.projectsModule.projects.filter(project => project.done).length;
+    this.undoneProjects = this.projectsModule.projects.filter(project => !project.done)
+}
 
 }
