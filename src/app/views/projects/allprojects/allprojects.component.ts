1db99f522026b6bfcde08397c254e5e22ad4d442
BhMedRayen <rayenmohamed2310@gmail.com>
2024-03-27 22:44:08 +0100

    update all projects

--- src/app/views/projects/allprojects/allprojects.component.ts
+++ src/app/views/projects/allprojects/allprojects.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ProjectsModule } from '../../models/project/projects.module';
 import { CommonModule } from '@angular/common';
 @Component({
@@ -8,11 +8,12 @@ import { CommonModule } from '@angular/common';
   templateUrl: './allprojects.component.html',
   styleUrl: './allprojects.component.scss'
 })
-export class AllprojectsComponent {
+export class AllprojectsComponent implements OnInit{
  
   allProjects: any[] = [];
   showAllProjects: boolean = false;
   displayedProjects: number = 6;
+  projectsWithTasks: any[] = [];
   constructor(private projectsModule: ProjectsModule) {
     this.allProjects = projectsModule.projects;
   }
@@ -23,4 +24,14 @@ export class AllprojectsComponent {
       this.displayedProjects = 6; 
     }
   }
+  ngOnInit(): void {
+    this.projectsWithTasks = this.projectsModule.projects.map(project => ({
+      ...project,
+      allTasks: project.backlog?.allTasks || 0, 
+      doneTasks: project.backlog?.doneTasks || 0 
+    }));
+
+
+  }
+  
 }
