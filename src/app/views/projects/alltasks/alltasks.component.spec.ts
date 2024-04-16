8e77238b1e54f29dd9cf7e5fcd28b65f754f39e2
BhMedRayen <rayenmohamed2310@gmail.com>
2024-04-17 00:25:26 +0100

    create tasks component

---
+++ src/app/views/projects/alltasks/alltasks.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { AlltasksComponent } from './alltasks.component';
+
+describe('AlltasksComponent', () => {
+  let component: AlltasksComponent;
+  let fixture: ComponentFixture<AlltasksComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [AlltasksComponent]
+    })
+    .compileComponents();
+    
+    fixture = TestBed.createComponent(AlltasksComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/views/projects/alltasks/alltasks.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-alltasks',
+  standalone: true,
+  imports: [],
+  templateUrl: './alltasks.component.html',
+  styleUrl: './alltasks.component.scss'
+})
+export class AlltasksComponent {
+
+}
