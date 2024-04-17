9473e81abb7d11edb95de4e8b7ea47e2eeb4a74b
BhMedRayen <rayenmohamed2310@gmail.com>
2024-04-17 14:39:32 +0100

    create all tasks

--- src/app/views/projects/alltasks/alltasks.component.ts
+++ src/app/views/projects/alltasks/alltasks.component.ts
@@ -1,12 +1,35 @@
-import { Component } from '@angular/core';
+  import { Component, OnInit } from '@angular/core';
+  import { ActivatedRoute } from '@angular/router';
+  import { TaskModule } from '../../models/task/tasks.module';
+  import {Task } from '../../models/task/task.model';
+  import { CommonModule } from '@angular/common';
 
-@Component({
-  selector: 'app-alltasks',
-  standalone: true,
-  imports: [],
-  templateUrl: './alltasks.component.html',
-  styleUrl: './alltasks.component.scss'
-})
-export class AlltasksComponent {
 
-}
+  @Component({
+    selector: 'app-alltasks',
+    standalone: true,
+    imports: [CommonModule],
+    templateUrl: './alltasks.component.html',
+    styleUrl: './alltasks.component.scss'
+  })
+  export class AlltasksComponent implements OnInit{
+    sprintId: number=0;
+    tasks: Task[] = [];
+
+
+    constructor(private route: ActivatedRoute) { }
+
+    ngOnInit(): void {
+      this.route.params.subscribe(params => {
+        this.sprintId = +params['id'];
+        console.log("Sprint ID:", this.sprintId); // Log sprint ID for debugging
+        console.log("All Tasks:", TaskModule.tasks); // Log all tasks for debugging
+        this.tasks = TaskModule.tasks.filter(task => task.sprintId === this.sprintId);
+        console.log("Filtered Tasks:", this.tasks); // Log filtered tasks for debugging
+      });
+    }
+    
+    
+    }
+
+
