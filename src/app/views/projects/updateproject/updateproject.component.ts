67fb27c8cb4dfe096e3337ab21264a703dbb0c05
BhMedRayen <rayenmohamed2310@gmail.com>
2024-05-11 23:54:50 +0100

    update project component

---
+++ src/app/views/projects/updateproject/updateproject.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-updateproject',
+  standalone: true,
+  imports: [],
+  templateUrl: './updateproject.component.html',
+  styleUrl: './updateproject.component.scss'
+})
+export class UpdateprojectComponent {
+
+}
