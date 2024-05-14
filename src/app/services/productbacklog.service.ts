131581b2d1db3e9cf1afd8604d41450032ddc78d
BhMedRayen <rayenmohamed2310@gmail.com>
2024-05-14 22:54:11 +0100

    product bcklog

---
+++ src/app/services/productbacklog.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import {Observable} from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductbacklogService {
+
+  private apiUrl ='http://localhost:8000/api/product-backlogs'
+  constructor(private http: HttpClient) { }
+
+  getBackLogById(backlogId : number) : Observable<any> {
+    return this.http.get<any>(`${this.apiUrl}/get-by-backlog-id/${backlogId}`)
+  }
+  
+  
+}
--- src/app/views/projects/bakclogdetails/bakclogdetails.component.ts
+++ src/app/views/projects/bakclogdetails/bakclogdetails.component.ts
@@ -1,65 +1,76 @@
-import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
-import { RouterLink } from '@angular/router';
-import { CommonModule } from '@angular/common';
-import { ProjectsService } from 'src/app/services/projects.service';
+  import { Component, OnInit } from '@angular/core';
+  import { ActivatedRoute } from '@angular/router';
+  import { RouterLink } from '@angular/router';
+  import { CommonModule } from '@angular/common';
+  import { ProjectsService } from 'src/app/services/projects.service';
+  import {ProductbacklogService} from 'src/app/services/productbacklog.service'
+  
+  @Component({
+    selector: 'app-bakclogdetails',
+    standalone: true,
+    imports: [RouterLink ,CommonModule],
+    templateUrl: './bakclogdetails.component.html',
+    styleUrl: './bakclogdetails.component.scss'
+  })
+  export class BakclogdetailsComponent implements OnInit {
 
- 
-@Component({
-  selector: 'app-bakclogdetails',
-  standalone: true,
-  imports: [RouterLink ,CommonModule],
-  templateUrl: './bakclogdetails.component.html',
-  styleUrl: './bakclogdetails.component.scss'
-})
-export class BakclogdetailsComponent implements OnInit {
+      backlogId: number = 0; 
+      backlog : any ;
+      productBacklogs : any ;
 
-    backlogId: number = 0; 
-    backlog : any ;
+
+  constructor(
+        private route: ActivatedRoute,
+        private projectsService: ProjectsService,
+        private productService : ProductbacklogService,
+    ) {}
 
 
 
-constructor(
-      private route: ActivatedRoute,
-      private projectsService: ProjectsService,
-   ) {}
+    
 
 
 
   
+    
+
+    ngOnInit(): void {
+
+      this.route.params.subscribe(params => {
+        this.backlogId=params['id'];
+        console.log("backlog id :",this.backlogId);
+        this.getBackLogById(this.backlogId);
+    })
+
+  }
 
 
+  getBackLogById(backlogId : number) : void {
+    this.projectsService.getBackLogById(backlogId).subscribe({
+      next : (response : any) => {
+        this.backlog=response.backlog
+        console.log("backlog :",this.backlog);
+        this.getProductBackLogs(this.backlog.id);
+      },
+      error : (error:any)=> {
+        console.log("error fetching backlog");
+        
+      },
+      complete : () => {
+        console.log("backlog fetched succefuly");
+        
+      }
+    })
+  }
 
- 
-  
+  getProductBackLogs(backlogId : number) : void {
+    this.productService.getBackLogById(backlogId).subscribe({
+      next:(response : any) => {
+        this.productBacklogs = response;
+        console.log("product back log ", this.productBacklogs);
+        
+      }
+    })
+  }
 
-  ngOnInit(): void {
-
-    this.route.params.subscribe(params => {
-      this.backlogId=params['id'];
-      console.log("backlog id :",this.backlogId);
-      this.getBackLogById(this.backlogId);
-  })
-
-}
-
-
-getBackLogById(backlogId : number) : void {
-  this.projectsService.getBackLogById(backlogId).subscribe({
-    next : (response : any) => {
-      this.backlog=response.backlog
-      console.log("backlog :",this.backlog);
-      
-    },
-    error : (error:any)=> {
-      console.log("error fetching backlog");
-      
-    },
-    complete : () => {
-      console.log("backlog fetched succefuly");
-      
-    }
-  })
-}
-
-}
+  }
