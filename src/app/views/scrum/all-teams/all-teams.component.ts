36401e81797a42e0c3e73cfe7b1339d5fac98577
BhMedRayen <rayenmohamed2310@gmail.com>
2024-05-14 01:53:54 +0100

    deleting ineeded lines

--- src/app/views/scrum/all-teams/all-teams.component.ts
+++ src/app/views/scrum/all-teams/all-teams.component.ts
@@ -2,11 +2,16 @@ import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { Team } from 'src/app/models/teams.model';
 import { TeamServiceService } from 'src/app/services/team-service.service';
-
+import  {CreateTeamComponent} from '../create-team/create-team.component'
+import { result } from 'lodash-es';
+import { MatDialog } from '@angular/material/dialog';
+import { RouterLink } from '@angular/router';
+import { RouterModule } from '@angular/router';
+import { DeleteTeamComponent} from '../delete-team/delete-team.component'
 @Component({
   selector: 'app-all-teams',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule,RouterLink,RouterModule],
   templateUrl: './all-teams.component.html',
   styleUrls: ['./all-teams.component.scss'] // Note the correct property name styleUrls
 })
@@ -15,7 +20,9 @@ export class AllTeamsComponent implements OnInit {
   teams : Team [] = []
 
   constructor (
-    private teamsService : TeamServiceService
+    private teamsService : TeamServiceService,
+    public dialog: MatDialog
+
   ) {}
 
 
@@ -28,15 +35,22 @@ export class AllTeamsComponent implements OnInit {
     this.teamsService.getAllTeams().subscribe({
       next: (response: any) => {
         this.teams = response.teams;
-        this.teams.forEach(team => { // Iterate over each team
-          team.users.forEach(user => { // Iterate over each user in the team
-            user.photo = 'http://localhost:8000' + user.photo; // Update the photo URL
+        this.teams.forEach(team => { 
+          team.users.forEach(user => { 
+            user.photo = 'http://localhost:8000' + user.photo; 
           });
         });
         console.log("teams : ", this.teams);
       }
     });
   }
-  
+
+
+openDeleteProjectComponent(teamId: number): void {
+  const dialogRef = this.dialog.open(DeleteTeamComponent,{
+    width:'500px', 
+    data : { id : teamId },
+  })
+}
 
 }
