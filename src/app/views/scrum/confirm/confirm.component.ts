7dfbbded50d408f789e84e83700730522274b79b
BhMedRayen <rayenmohamed2310@gmail.com>
2024-05-15 21:10:55 +0100

    change scrum master and add teamMember

---
+++ src/app/views/scrum/confirm/confirm.component.ts
@@ -0,0 +1,54 @@
+import { CommonModule } from '@angular/common';
+import { Component, Inject, OnInit } from '@angular/core';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+import { FormModule } from '@coreui/angular';
+import { TeamServiceService } from 'src/app/services/team-service.service';
+
+
+@Component({
+  selector: 'app-confirm',
+  standalone: true,
+  imports: [
+    CommonModule,
+    FormModule
+  ],
+  templateUrl: './confirm.component.html',
+  styleUrl: './confirm.component.scss'
+})
+export class ConfirmComponent implements OnInit {
+  loading : boolean = false;
+
+  constructor (
+    public dialogRef: MatDialogRef<ConfirmComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: { teamId : number ,userId : number  },
+    private teamsService : TeamServiceService
+
+  ) {}
+
+
+  onCancelClick(): void {
+    this.dialogRef.close();
+  }
+
+  ngOnInit(): void {
+  
+  }
+
+  assignAsScrumMaster() {
+    this.loading=true
+    this.teamsService.changeScrumMaster(this.data.teamId,this.data.userId).subscribe({
+      next : (response : any) => {
+        console.log("Team master assigned succefuly");
+        this.loading=false
+        
+      },
+      error : (error : any) =>{
+        this.loading=false;
+        console.log("error updating scrum master " , error);
+     
+      },
+    })
+    
+  }
+
+}
