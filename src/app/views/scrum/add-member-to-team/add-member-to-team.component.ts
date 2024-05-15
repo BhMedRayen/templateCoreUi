99e541bad2393365ac2c02430d485fcc7016e00d
BhMedRayen <rayenmohamed2310@gmail.com>
2024-05-15 21:10:45 +0100

    change scrum master and add teamMember

---
+++ src/app/views/scrum/add-member-to-team/add-member-to-team.component.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit } from '@angular/core';
+import { TeamServiceService } from 'src/app/services/team-service.service';
+
+@Component({
+  selector: 'app-add-member-to-team',
+  standalone: true,
+  imports: [],
+  templateUrl: './add-member-to-team.component.html',
+  styleUrl: './add-member-to-team.component.scss'
+})
+export class AddMemberToTeamComponent implements OnInit {
+
+constructor (
+    public teamService : TeamServiceService,
+) {}
+
+  ngOnInit(): void {
+   
+  }
+
+  
+
+  addTeamToMember(teamId: number, userIds: number[]): void {
+    this.teamService.addTeamMember(teamId, userIds).subscribe({
+      next: (response: any) => {
+        console.log('Team members added successfully:', response);
+      },
+      error: (error: any) => {
+        console.error('Error adding team members:', error);
+      
+      }
+    });
+  }
+
+  
+
+}
