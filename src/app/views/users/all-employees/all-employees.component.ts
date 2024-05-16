1a0dcdcf1c22d50d95ee337265087e6a36231222
BhMedRayen <rayenmohamed2310@gmail.com>
2024-05-16 15:07:55 +0100

    all employees component

---
+++ src/app/views/users/all-employees/all-employees.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-all-employees',
+  standalone: true,
+  imports: [],
+  templateUrl: './all-employees.component.html',
+  styleUrl: './all-employees.component.scss'
+})
+export class AllEmployeesComponent {
+
+}
