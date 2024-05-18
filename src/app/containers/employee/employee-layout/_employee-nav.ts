f779f3d8f1bb41e5616c4c18aff455caa2b02ab3
BhMedRayen <rayenmohamed2310@gmail.com>
2024-05-18 23:37:32 +0100

    employee nav

--- src/app/containers/employee/employee-layout/_employee-nav.ts
+++ src/app/containers/employee/employee-layout/_employee-nav.ts
@@ -3,6 +3,43 @@ import { INavData } from '@coreui/angular';
 export const navItems: INavData[] = [
 
   {
+    name : 'Projects',
+    url : '/projects',
+    iconComponent : {name : 'cil-apps'},
+    children : [
+      {
+        name : 'Active Projects',
+        url : 'projects/active-projects'
+      },
+      {
+        name : 'Done Projects',
+        url : 'projects/done-projects'
+      }     
+    ]
+  },
+  {
+    name : 'My Team Projects',
+    url : '/team-projects',
+    iconComponent : {name : 'cil-apps'},
+    children : [
+      {
+        name : 'Projects',
+        url : 'projects/projects'
+      }     
+    ]
+  },
+  {
+    name: 'Scrum',
+    url: '/scrums',
+    iconComponent: { name: 'cil-apps' },
+    children : [
+      {
+        name :'My Team',
+        url  :'messages/my-team'
+      },
+    ]
+  },
+  {
     name: 'Messages',
     url: '/messages',
     iconComponent: { name: 'cil-apps' },
@@ -12,7 +49,9 @@ export const navItems: INavData[] = [
         url  :'messages/inbox'
       },
     ]
-  }
+  },
+ 
+ 
 
 
 ];
