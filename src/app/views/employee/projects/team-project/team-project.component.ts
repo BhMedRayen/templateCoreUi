dd4cfddf35dcf86e3d9f3380224073a22f355b7b
BhMedRayen <rayenmohamed2310@gmail.com>
2024-05-22 13:01:03 +0100

    get projects and teams for user

--- src/app/views/employee/projects/team-project/team-project.component.ts
+++ src/app/views/employee/projects/team-project/team-project.component.ts
@@ -1,10 +1,66 @@
-import { Component } from '@angular/core';
+// team-project.component.ts
+
+import { Component, OnInit } from '@angular/core';
+import { TeamServiceService } from 'src/app/services/team-service.service';
 
 @Component({
   selector: 'app-team-project',
   templateUrl: './team-project.component.html',
-  styleUrl: './team-project.component.scss'
+  styleUrls: ['./team-project.component.scss']
 })
-export class TeamProjectComponent {
+export class TeamProjectComponent implements OnInit {
+ 
+  emplId: number = 0;
+  user: any;
+  teams: any[] = [];
+  projectsByTeam: any = {};
 
+  constructor(private teamService: TeamServiceService) {}
+
+  ngOnInit(): void {
+    const userData = localStorage.getItem('employee');
+    if (userData) {
+      this.user = JSON.parse(userData);
+      console.log("user auth ", this.user);
+    } else {
+      console.error('User data not found in local storage.');
+    }
+    this.emplId = this.user.id;
+    this.getUserTeams();
+  }
+
+  getUserTeams(): void {
+    this.teamService.getUserTeams(this.emplId).subscribe({
+      next: (response: any) => {
+        this.teams = response.teams;
+        console.log('teams for the auth employee', this.teams);
+        console.log('Teams:', this.teams);
+
+        this.teams.forEach(team => {
+          console.log('Team:', team);
+
+          if (team.team.id) { 
+            this.getProjectsByTeam(team.team.id); 
+          } else {
+            console.error('Team ID is undefined or null');
+          }
+        });
+      },
+      error: (err: any) => {
+        console.error('Error fetching teams', err);
+      }
+    });
+  }
+
+  getProjectsByTeam(teamId: number): void {
+    this.teamService.getProjectByTeamId(teamId).subscribe({
+      next: (response: any) => {
+        this.projectsByTeam[teamId] = response.projects;
+        console.log(`projects for team ${teamId}`, response.projects);
+      },
+      error: (err: any) => {
+        console.error(`Error fetching projects for team ${teamId}`, err);
+      }
+    });
+  }
 }
