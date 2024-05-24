e3587410902340953b0c8cfd03e0d5641bb8a831
BhMedRayen <rayenmohamed2310@gmail.com>
2024-05-24 16:13:09 +0100

    create sprint and get sprints

---
+++ src/app/services/sprint-service.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SprintServiceService {
+
+  private apiUrl = 'http://localhost:8000/api/sprints';
+
+  constructor(
+    private http: HttpClient
+    ) { }
+
+    createSprint(sprintData : any) : Observable <any> {
+      return this.http.post(this.apiUrl+'create',sprintData);
+    }
+    
+    getSprintsByProjectId(projectId : number) : Observable <any> {
+      return this.http.get(`${this.apiUrl}/backlogs/${projectId}`)
+    }
+}
