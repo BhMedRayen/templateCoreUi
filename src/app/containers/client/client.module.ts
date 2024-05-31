4eb29027cdeba255753cf2f1881ceb2813e55a76
BhMedRayen <rayenmohamed2310@gmail.com>
2024-05-31 21:39:50 +0100

    client contract component

--- src/app/containers/client/client.module.ts
+++ src/app/containers/client/client.module.ts
@@ -1,23 +1,21 @@
 import { NgModule } from '@angular/core';
-import { ClientRoutingModule } from './client-routing.module';
-import {ClientLayoutComponent} from './client-layout/client-layout.component'
 import { CommonModule } from '@angular/common';
-
+import { RouterModule } from '@angular/router';
+import { ClientLayoutComponent } from './client-layout/client-layout.component';
+import { ClientRoutingModule } from './client-routing.module';
+import { ClientContractsComponent } from './client-contracts/client-contracts.component';
+import { ClientWelcomeComponent } from './client-layout/client-welcome.component';
 
 @NgModule({
-
-    declarations : [
-
-        ClientLayoutComponent,
-        
-    ], 
-    imports : [
-        ClientRoutingModule,
-        CommonModule,
-    ],
-
+  declarations: [
+    ClientLayoutComponent,
+    ClientContractsComponent,
+    ClientWelcomeComponent
+  ],
+  imports: [
+    CommonModule,
+    ClientRoutingModule,
+    RouterModule
+  ],
 })
-
-export class ClientModule {
-
-}
+export class ClientModule { }
