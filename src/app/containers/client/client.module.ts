2ec0bbe9fead0b140df66ef321b4e2efb6c94e50
BhMedRayen <rayenmohamed2310@gmail.com>
2024-06-01 00:44:44 +0100

    chat routing

--- src/app/containers/client/client.module.ts
+++ src/app/containers/client/client.module.ts
@@ -5,17 +5,21 @@ import { ClientLayoutComponent } from './client-layout/client-layout.component';
 import { ClientRoutingModule } from './client-routing.module';
 import { ClientContractsComponent } from './client-contracts/client-contracts.component';
 import { ClientWelcomeComponent } from './client-layout/client-welcome.component';
+import { ChatComponent } from './chat/chat.component';
 
 @NgModule({
   declarations: [
     ClientLayoutComponent,
     ClientContractsComponent,
-    ClientWelcomeComponent
+    ClientWelcomeComponent,
+    ChatComponent
+    
   ],
   imports: [
     CommonModule,
     ClientRoutingModule,
-    RouterModule
+    RouterModule,
+    
   ],
 })
 export class ClientModule { }
