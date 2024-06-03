352e541eb3070b5140f82f1522aa7fad5d093474
BhMedRayen <rayenmohamed2310@gmail.com>
2024-06-03 21:53:57 +0100

    get the user from local storage

--- src/app/containers/default-layout/default-header/default-header.component.ts
+++ src/app/containers/default-layout/default-header/default-header.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 import { ClassToggleService, HeaderComponent } from '@coreui/angular';
 import {AuthService} from "../../../services/auth-service";
@@ -8,13 +8,14 @@ import {Router} from "@angular/router";
   selector: 'app-default-header',
   templateUrl: './default-header.component.html',
 })
-export class DefaultHeaderComponent extends HeaderComponent {
+export class DefaultHeaderComponent extends HeaderComponent implements OnInit {
 
   @Input() sidebarId: string = "sidebar";
 
   public newMessages = new Array(4)
   public newTasks = new Array(5)
   public newNotifications = new Array(5)
+  userImage : String = ''
 
   constructor(
     private classToggler: ClassToggleService,
@@ -24,6 +25,19 @@ export class DefaultHeaderComponent extends HeaderComponent {
   ) {
     super();
   }
+  ngOnInit(): void {
+    this.getUserPhoto()
+   
+  }
+
+  getUserPhoto() {
+    const userData = localStorage.getItem('product_owner');
+    if (userData) {
+      const user = JSON.parse(userData);
+      this.userImage = 'http://localhost:8000'+user.photo;
+    }
+  }
+
 
   logout() {
     this.authService.logout(); 
