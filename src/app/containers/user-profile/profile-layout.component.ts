90b029a1f8b7582c77192a607f5d4bf3a842cd8b
BhMedRayen <rayenmohamed2310@gmail.com>
2024-06-05 23:00:57 +0100

    profile

--- src/app/containers/user-profile/profile-layout.component.ts
+++ src/app/containers/user-profile/profile-layout.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/services/auth-service';
 import { UsersService } from 'src/app/services/users.service';
 
 
@@ -23,7 +25,9 @@ export class ProfileLayoutComponent implements OnInit {
   parsedSkills: string[] = [];
   
   constructor(
-    private userService : UsersService
+    private userService : UsersService,
+    private authService : AuthService,
+    private router: Router 
   ) {}
   ngOnInit(): void {
     this.loadUserIdFromLocalStorage()
@@ -65,6 +69,10 @@ export class ProfileLayoutComponent implements OnInit {
     }
   }
 
+  logout() {
+    this.authService.logout(); 
+    this.router.navigate(['/auth/login']); 
+  }
   
 
   getUserById() : void {
