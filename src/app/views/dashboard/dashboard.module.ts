ab068efd01e7656b551730ac9cbc3f8f09d23f69
BhMedRayen <rayenmohamed2310@gmail.com>
2024-06-06 21:32:55 +0100

    step by step to the last commit

--- src/app/views/dashboard/dashboard.module.ts
+++ src/app/views/dashboard/dashboard.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
-
+import { DashboardRoutingModule } from './dashboard-routing.module'
 import { DashboardComponent } from './dashboard.component';
 
 @NgModule({
   imports: [
-  ],
+    DashboardRoutingModule
+  ],
   declarations: [DashboardComponent]
 })
 export class DashboardModule {
